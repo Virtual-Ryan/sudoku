9e61ff72914d688da31cf155e91ec147bf4f7e71
Virtual-Ryan <ryanpcarlson@gmail.com>
2022-06-23 11:58:30 -0700

    2nd Test

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,10 @@
-# Test
-print('Hello Sudoku!')
-print('Goodbye Sudoku')
-print('Please work...')
+# Sudoku Solver - Recommended as a good beginener project
+# V1 is a from scratch build with google as my only friend.
+
+name = input('If you are not the Lizard King, then who the heck are you?: ')
+
+i = 0
+
+while i <= 20:
+    print(name)
+    i += 1
